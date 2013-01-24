d7ad7acd59a281dac8bf6d94438ccd3836d27c98
Ross Nanopoulos <rdn21@case.edu>
2013-01-23 21:14:56 -0500

    Added EndTime, DatePicker

--- gQuery.py
+++ gQuery.py
@@ -9,6 +9,6 @@ def getEvents(limit, pageNum, user):
 			# query+="AND verified = TRUE "
 		query += "Limit " +(str)(limit*pageNum)+", " + (str)(limit)
 		events = Event.gql(query, datetime.now()-timedelta(hours=2)).run()
-		events = sorted(events, key=lambda event: event.dateStart) 
+		events = sorted(events, key=lambda event: event.dateStart)
 		return events		
 
